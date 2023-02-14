9807f8324b125543d14424335b520161b08e8ed1
deevsky69 <adivafiqrinugraha69@gmail.com>
2023-02-14 10:02:48 +0700

    menambahkan case menjadi 4 buah case dan mengubah pom.xml

---
+++ src/test/java/AUTest2.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Pluto-07
+ */
+public class AUTest2 {
+    WebDriver driver = null;
+    public AUTest2() {
+    }
+    @BeforeTest
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @Test
+    public void secondTry(){
+        driver.get("https://localhost/index.php");
+        driver.findElement(By.id("username")).sendKeys("adiva");
+        driver.findElement(By.id("password")).sendKeys("fiqri");
+        driver.findElement(By.xpath("/html/body/div/form/input[3]")).click();
+    }
+    
+    @AfterTest
+    public void tearDown(){
+        driver.close();
+        driver.quit();
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+}
